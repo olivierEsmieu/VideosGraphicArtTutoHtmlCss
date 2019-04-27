45dd06378f852b1cdd7154a24ef779078a2c6a77
olivier Esmieu-Fournel <olivier.esmieu@gmail.com>
2019-04-27 08:08:25 +0200

    my first commit
    mon premier commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img width="25%" src="./assets/logo.png">
-    <HelloWorld/>
+    <h1>h1</h1>
+    <h2>h1</h2>
+    <p>
+      <strong>Lorem &nbsp; &nbsp;&nbsp; ipsum dolor,</strong>
+      <em>amet consectetur</em> adipisicing elit.
+      <br>Alias earum ab quaerat fugit itaque aliquam eveniet inventore soluta ipsam distinctio, reiciendis a, praesentium suscipit eaque obcaecati iure vitae ex nihil!
+    </p>
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, libero. Ipsam et provident vel quo possimus nemo dignissimos dolorem numquam illum quae facilis dolorum distinctio reiciendis, modi adipisci quas expedita!</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
-};
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
