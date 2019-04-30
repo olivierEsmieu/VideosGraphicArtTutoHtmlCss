74d3c60d6630f97e0cd84810f8e6be43bb434aca
olivier Esmieu-Fournel <olivier.esmieu@gmail.com>
2019-04-30 12:47:46 +0200

    commit2
    2e commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,44 @@
 <template>
-  <div id="app">
-    <h1>h1</h1>
-    <h2>h1</h2>
+  <div>
+    <h1>Restaurant Le Fiasco</h1>
+    <nav>
+      <ul>
+        <a href="index.html" title="index">
+          <li>Index</li>
+        </a>
+        <a href="menu.html" title="menu">
+          <li>Menu</li>
+        </a>
+        <a href="contact.html" title="contact">
+          <li>Contact</li>
+        </a>
+      </ul>
+    </nav>
+    <!-- <iframe src="menu.html" width="800" height="600"></iframe> -->
     <p>
-      <strong>Lorem &nbsp; &nbsp;&nbsp; ipsum dolor,</strong>
-      <em>amet consectetur</em> adipisicing elit.
-      <br>Alias earum ab quaerat fugit itaque aliquam eveniet inventore soluta ipsam distinctio, reiciendis a, praesentium suscipit eaque obcaecati iure vitae ex nihil!
+      <a href="../src/assets/le_fiasco.jpg" target="_blank" rel="noopener noreferrer">
+        <img src="../src/assets/le_fiasco_small.jpg" alt="entrée du Restaurant">
+      </a>
+      
+      <strong>Lorem &nbsp; &nbsp;&nbsp; ipsum dolor,</strong> amet consectetur adipisicing elit.
+      <a href="#cuisine">Cuisine</a> earum ab quaerat
+      <a href="menu.html#plats-chauds">avec des plats chauds</a>
+      <br>reiciendis a, praesentium suscipit eaque obcaecati iure vitae ex nihil!
     </p>
     <hr>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, libero. Ipsam et provident vel quo possimus nemo dignissimos dolorem numquam illum quae facilis dolorum distinctio reiciendis, modi adipisci quas expedita!</p>
+    <a href="menu.html">Menu</a>
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, libero.Ipsam et provident vel quo possimus nemo dignissimos dolorem numquam
+        illum quae facilis dolorum distinctio reiciendis, modi adipisci quas expedita!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, libero.Ipsam et provident vel quo possimus nemo dignissimos dolorem numquam
+        illum quae facilis dolorum distinctio reiciendis, modi adipisci quas expedita!
+      </p>
+      <h2 id="cuisine">la cuisine</h2>
+      <img src="../src/assets/logo.png" alt="logo Vue" width="100" height="100">
+    </div>
   </div>
 </template>
 
